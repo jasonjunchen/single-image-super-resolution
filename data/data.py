da17a56b977cb6348b4334510e73619e22b0872b
jasonjunchen <69047536+jasonjunchen@users.noreply.github.com>
2020-08-21 12:12:50 +0800

    Update data.py

--- data/data.py
+++ data/data.py
@@ -189,3 +189,4 @@ def download_archive(file, target_dir, extract=True):
     source_url = f'http://data.vision.ee.ethz.ch/cvl/DIV2K/{file}'
     target_dir = os.path.abspath(target_dir)
     tf.keras.utils.get_file(file, source_url, cache_subdir=target_dir, extract=extract)
+    os.remove(os.path.join(target_dir, file))
